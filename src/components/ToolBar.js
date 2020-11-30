6763ecf8ed2bd25aa5363b4a793150cf0e60d3d6
Tdallau <tim@dallau.com>
2020-11-30 13:14:07 +0100

    replaced title

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,56 +1,62 @@
 import React from 'react';
-import {View, TouchableOpacity, Text} from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
 import PropTypes from 'prop-types';
-import {toolBarStyles} from "../calendar/style";
+import { toolBarStyles } from '../calendar/style';
 
-const ToolBar = (
-    {
-        style,
-        titleText,
-        titleStyle,
-        cancelStyle,
-        confirmStyle,
-        cancelText,
-        confirmText,
-        cancel,
-        confirm,
-        cancelDisabled,
-        confirmDisabled,
-    }
-) => (
+const ToolBar = ({
+  style,
+  titleText,
+  titleStyle,
+  cancelStyle,
+  confirmStyle,
+  cancelText,
+  confirmText,
+  cancel,
+  confirm,
+  cancelDisabled,
+  confirmDisabled,
+}) => (
+  <View>
+    {titleText ? (
+      <Text style={[toolBarStyles.title, titleStyle]}>{titleText}</Text>
+    ) : null}
     <View style={[toolBarStyles.view, style]}>
-        <TouchableOpacity
-            title={cancelText}
-            disabled={cancelDisabled}
-            activeOpacity={cancelStyle && cancelStyle.activeOpacity || 0.2}
-            onPress={() => cancel && typeof cancel === 'function' && cancel()}
-        >
-            <Text style={[toolBarStyles.cancelText, cancelStyle]}>{cancelText}</Text>
-        </TouchableOpacity>
-        {titleText ? <Text style={[toolBarStyles.title, titleStyle]}>{titleText}</Text> : null}
-        <TouchableOpacity
-            title={confirmText}
-            disabled={confirmDisabled}
-            activeOpacity={confirmStyle && confirmStyle.activeOpacity || 0.2}
-            onPress={() => confirm && typeof confirm === 'function' && confirm()}
-        >
-            <Text style={[toolBarStyles.confirmText, confirmStyle]}>{confirmText}</Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        title={cancelText}
+        disabled={cancelDisabled}
+        activeOpacity={(cancelStyle && cancelStyle.activeOpacity) || 0.2}
+        onPress={() => cancel && typeof cancel === 'function' && cancel()}
+      >
+        <Text style={[toolBarStyles.cancelText, cancelStyle]}>
+          {cancelText}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        title={confirmText}
+        disabled={confirmDisabled}
+        activeOpacity={(confirmStyle && confirmStyle.activeOpacity) || 0.2}
+        onPress={() => confirm && typeof confirm === 'function' && confirm()}
+      >
+        <Text style={[toolBarStyles.confirmText, confirmStyle]}>
+          {confirmText}
+        </Text>
+      </TouchableOpacity>
     </View>
+  </View>
 );
 
 ToolBar.propTypes = {
-    style: PropTypes.any,
-    cancelStyle: PropTypes.any,
-    confirmStyle: PropTypes.any,
-    titleStyle: PropTypes.any,
-    titleText: PropTypes.string,
-    cancelText: PropTypes.string,
-    confirmText: PropTypes.string,
-    cancel: PropTypes.func,
-    confirm: PropTypes.func,
-    cancelDisabled: PropTypes.bool,
-    confirmDisabled: PropTypes.bool,
+  style: PropTypes.any,
+  cancelStyle: PropTypes.any,
+  confirmStyle: PropTypes.any,
+  titleStyle: PropTypes.any,
+  titleText: PropTypes.string,
+  cancelText: PropTypes.string,
+  confirmText: PropTypes.string,
+  cancel: PropTypes.func,
+  confirm: PropTypes.func,
+  cancelDisabled: PropTypes.bool,
+  confirmDisabled: PropTypes.bool,
 };
 
 export default ToolBar;
